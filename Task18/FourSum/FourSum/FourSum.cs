f3698ee9d9242dcfac9a93805f711f52ca859112
Aliaksei Poliak <aliakseipv@gmail.com>
2022-10-21 10:47:46 -0400

    Adding Solution FoursNumbers

---
+++ Task18/FourSum/FourSum/FourSum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourSum
+{
+    internal class FourSum
+    {
+        public IList<IList<int>> SumOfFours(int[] nums, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            if (nums.Length < 4)
+                return result;
+
+            Array.Sort(nums);
+            int count = 0;
+
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                for (int j = i + 1; j < nums.Length - 2; j++)
+                {
+                    int left = j + 1;
+                    int right = nums.Length - 1;
+
+                    while (left < right)
+                    {
+                        long sum = (long)(nums[i] + nums[j]) + (long)(nums[left] + nums[right]);
+
+                        if (sum == target)
+                        {
+                            List<int> solution = new List<int> { nums[i], nums[j], nums[left], nums[right] };
+                            foreach (var item in result)
+                            {
+                                if (item.SequenceEqual(solution))
+                                {
+                                    count++;
+                                    break;
+                                }
+                            }
+                            if (count == 0)
+                                result.Add(solution);
+                            else count = 0;
+                            left++;
+                            right--;
+                        }
+                        else if (sum < target)
+                            left++;
+                        else                        
+                            right--;                        
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Task18/FourSum/FourSum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace FourSum
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 0, -1, 0, -2, 2, 3, 4, -3, 2, -5 };
+            var target = 0;
+
+            FourSum fourSum = new FourSum();
+            fourSum.SumOfFours(nums, target);
+        }
+    }
+}
