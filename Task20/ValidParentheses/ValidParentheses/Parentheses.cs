c0f272d2b587e3ab30d79af7c7fee9eda220d4b4
Aliaksei Poliak <aliakseipv@gmail.com>
2022-10-24 04:41:12 -0400

    Adding Solution ValidParentheses

---
+++ Task20/ValidParentheses/ValidParentheses/Parentheses.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ValidParentheses
+{
+    internal class Parentheses
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (c == '(')
+                    stack.Push(')');
+                else if (c == '{')
+                    stack.Push('}');
+                else if (c == '[')
+                    stack.Push(']');
+                else if (stack.Count() == 0 || stack.Pop() != c)
+                    return false;
+            }
+
+            return stack.Count() == 0;
+        }
+    }
+}
---
+++ Task20/ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Parentheses();
+
+            Console.WriteLine(p.IsValid("({[]})"));
+            Console.WriteLine(p.IsValid("({[()]})"));
+            Console.WriteLine(p.IsValid("(([]})"));
+            Console.WriteLine(p.IsValid("()[]{}"));
+        }
+    }
+}
