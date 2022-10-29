9f9c7bcd9dc537adf61a708a4f4ff0542104738b
Aliaksei Poliak <aliakseipv@gmail.com>
2022-10-29 07:00:08 -0400

    Adding Solution RemoveDuplicates

---
+++ Task26/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var result = new Solution();
+
+            Console.WriteLine(result.RemoveDuplicates(input));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task26/RemoveDuplicates/RemoveDuplicates/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    internal class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {            
+            int temp = nums[0];
+            int count = 0;
+
+            for (int i = 1, j = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != temp)
+                {
+                    temp = nums[i];
+                    nums[j] = nums[i];
+                    j = i;
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
