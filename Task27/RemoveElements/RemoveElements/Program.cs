0831c8241d0282af5a8d88ee082370af7646211d
Aliaksei Poliak <aliakseipv@gmail.com>
2022-10-29 09:16:48 -0400

    Adding Solution RemoveElements

---
+++ Task27/RemoveElements/RemoveElements/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RemoveElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.RemoveElement(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+        }
+    }
+}
---
+++ Task27/RemoveElements/RemoveElements/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RemoveElements
+{
+    internal class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int j = nums.Length - 1;
+
+            for (int i = 0; i <= j; i++)
+            {
+
+                if (nums[i] == val && nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    j--;
+                }
+                else if (nums[i] == val && nums[j] == val)
+                {
+                    j--;
+                    i--;
+                }
+            }
+            return j + 1;
+        }
+    }
+}
