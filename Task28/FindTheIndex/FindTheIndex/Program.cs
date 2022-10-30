3010b4483bba4c974eb3805676e7e5964aeedc6b
Aliaksei Poliak <aliakseipv@gmail.com>
2022-10-30 02:55:01 -0400

    Find the Index of the First Occurrence in a String

---
+++ Task28/FindTheIndex/FindTheIndex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.StrStr2("sadbutsad", "sad"));
+            Console.WriteLine(solution.StrStr2("leetcode", "leeto"));
+            Console.WriteLine(solution.StrStr2("mississippi", "issip"));
+            Console.WriteLine(solution.StrStr2("aabaabbbaabbbbabaaab", "abaa"));
+            
+
+        }
+    }
+}
---
+++ Task28/FindTheIndex/FindTheIndex/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FindTheIndex
+{
+    public class Solution
+    {
+        public int StrStr2(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+        public int StrStr(string haystack, string needle)
+        {           
+            int result = -1;
+
+            if(haystack.Length < needle.Length)
+                return result;
+
+            string temp = string.Empty;
+            int index = 0;
+
+            while (index < haystack.Length)
+            {
+                int j = 0;
+                for (int i = index; i < haystack.Length; i++)
+                {
+                    if (haystack[i] == needle[j])
+                    {
+                        temp += needle[j];
+                        if (temp.Equals(needle))
+                        {
+                            result = i - (needle.Length - 1);
+                            return result;
+                        }
+                        j++;
+                    }
+                    else break;
+
+                }
+                temp = string.Empty;
+                index++;
+            }
+            
+            return result;
+        }
+    }
+}
