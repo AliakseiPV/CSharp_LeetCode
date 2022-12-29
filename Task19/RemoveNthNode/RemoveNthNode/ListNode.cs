c10437357df1c69c11bb4d11c9d425213b2458ee
Aliaksei Poliak <aliakseipv@gmail.com>
2022-12-29 09:43:03 -0500

    Task19 RemoveNthNode Solution

---
+++ Task19/RemoveNthNode/RemoveNthNode/ListNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNthNode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var result = head;
+
+            var previous = result;
+            var current = head;
+            var last = head;
+
+            while (n > 1)
+            {
+                last = last.next;
+                n--;
+            }
+
+            while (last.next != null)
+            {
+                previous = current;
+                current = current.next;
+                last = last.next;
+            }
+
+            previous.next = current.next;
+            current.next = null;
+
+            return result;
+        }
+    }
+}
---
+++ Task19/RemoveNthNode/RemoveNthNode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNthNode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(3);
+            listNode.next.next.next = new ListNode(4);
+            listNode.next.next.next.next = new ListNode(5);
+
+            var a = listNode.RemoveNthFromEnd(listNode, 2);
+        }
+    }
+}
